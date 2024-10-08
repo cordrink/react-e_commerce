c9bbc1159cb52543940c92bf25427d2742bd7c47
cordrink <masodavictorjudicael@gmail.com>
2024-10-08 23:04:25 +0100

    Creation de la fonctionnalite d'ajout d'un produit au panier

--- src/Pages/ProductShowcase/ProductShowcase.js
+++ src/Pages/ProductShowcase/ProductShowcase.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "./ProductShowcase.css"
 import {useParams} from "react-router-dom";
 import inventory from "../../data/inventory";
+import {useDispatch} from "react-redux";
+import {addItem} from "../../store/cart/cart-slice";
 
 function ProductShowcase(props) {
 
@@ -9,25 +11,61 @@ function ProductShowcase(props) {
 
     const {id} = useParams()
 
+    const dispatch = useDispatch();
+
+    const addingInfo = useRef();
+    let timerInfo;
+    let display = true;
+
     const productClicked = inventory.findIndex(object => object.title.replace(/\s+/g, "").trim() === id)
 
     const updateMugs = e => {
         setNbMugs(Number(e.target.value))
     }
 
+    const addToCart = e => {
+        e.preventDefault();
+
+        const itemAdded = {
+            ...inventory[productClicked],
+            quantity: nbMugs,
+        };
+
+        dispatch(addItem(itemAdded));
+
+        addingInfo.current.innerText = "Ajoute au panier";
+
+        if (display) {
+            display = false;
+            timerInfo = setTimeout(() => {
+                addingInfo.current.innerText = "";
+                display = true;
+            }, 500);
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(timerInfo);
+        }
+    }, []);
+
     return (
         <div className={"showcase"}>
             <div className="container-img-showcase">
-                <img className={"img-showcase"} src={process.env.PUBLIC_URL + `/images/${inventory[productClicked].img}.png`} alt="image de produit"/>
+                {/* process.env.PUBLIC_URL : facilite la localisation de l'imge en mode production */}
+                <img className={"img-showcase"}
+                     src={process.env.PUBLIC_URL + `/images/${inventory[productClicked].img}.png`}
+                     alt="illustration de produit"/>
             </div>
             <div className="product-infos">
                 <h2>{inventory[productClicked].title}</h2>
                 <p>Prix: {inventory[productClicked].price} </p>
-                <form>
+                <form onSubmit={addToCart}>
                     <label htmlFor="quantity">Quantite`</label>
-                    <input type="number" id={"quantity"} value={nbMugs} onChange={updateMugs} />
+                    <input type="number" id={"quantity"} value={nbMugs} onChange={updateMugs}/>
                     <button>Ajouter au panier</button>
-                    <span className="adding-info"></span>
+                    <span ref={addingInfo} className="adding-info"></span>
                 </form>
             </div>
         </div>
--- src/store/cart/cart-slice.js
+++ src/store/cart/cart-slice.js
@@ -6,7 +6,30 @@ export const cartSlice = createSlice({
         cart: []
     },
     reducers : {
-        addItem: (state, action) => {},
+        addItem: (state, action) => {
+            const indexItemAdd = state.cart.findIndex(obj => obj.id === action.payload.id);
+
+            if (indexItemAdd !== -1) {
+                const updateQuantity = {
+                    ...state.cart[indexItemAdd],
+                    quantity: action.payload.quantity + state.cart[indexItemAdd].quantity
+                }
+
+                const newArr = [...state.cart]
+                newArr.splice(indexItemAdd, 1, updateQuantity);
+
+                state.cart = newArr;
+            } else {
+                const newArr = [...state.cart];
+                newArr.push(action.payload);
+                state.cart = newArr;
+            }
+            console.log(state.cart);
+        },
         updateItem: (state, action) => {},
     }
 })
+
+const {addItem, updateItem} = cartSlice.actions;
+
+export {addItem, updateItem};
